0e89ef8679c58d551d029cff9685a449dcaa154b
Armadillo <s.nili80@outlook.com>
2021-08-03 09:47:00 +0430

    Update HelloWorld.cs

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -1,10 +1,12 @@
+using System;
+
 namespace Program
 {
     class Program
     {
         static void main(string[] args)
         {
-
+            Console.WriteLine("Khers e mehraboon");
         }
     }
 }
